2eea8183459f114486259355b8c45ef4dd524e0b
AilyGucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-07-11 04:03:06 +0000

    this is my domain code generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["my", "some", "our"];
+let adjective = ["great", "funny", "wonderful"];
+let noun = ["adventure", "catVideos", "dog"];
+let domainName = ["net", "com", "org"];
+
+function getDomainName() {
+  let result = "";
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+    for (
+      let adjectiveIndex = 0;
+      adjectiveIndex < adjective.length;
+      adjectiveIndex++
+    ) {
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+        for (
+          let domainNameIndex = 0;
+          domainNameIndex < domainName.length;
+          domainNameIndex++
+        ) {
+          let domainNameConcat =
+            pronoun[pronounIndex] +
+            "." +
+            adjective[adjectiveIndex] +
+            "." +
+            noun[nounIndex] +
+            "." +
+            domainName[domainNameIndex];
+          result = result + domainNameConcat + "<br>";
+        }
+      }
+    }
+  }
+
+  document.getElementById("result").innerHTML = result;
+  document.getElementById("btn").style.display = "none";
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("btn").addEventListener("click", getDomainName);
 };
